f651617bcb102262f79ce1d401f60954253c202a
Noah Hobbs <mlzgphoenix@gmail.com>
2021-02-11 15:19:45 -0600

    Fixed a ton of bugs and finished my edit class

--- src/controller/DetailsHelper.java
+++ src/controller/DetailsHelper.java
@@ -9,10 +9,12 @@ import javax.persistence.TypedQuery;
 
 import model.Details;
 
+
 public class DetailsHelper {
 	static EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("carsShoppingProject");
 	
-	public void insertItem(Details det) {
+	public void insertCar(Details det) {
+		// This method inserts cars into my database
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
 		em.persist(det);
@@ -20,14 +22,16 @@ public class DetailsHelper {
 		em.close();
 	}
 	
-	public List<Details> showAllItems(){
+	public List<Details> showAllCars(){
+		// this method gets all items inside my database puts them in a list and then returns them
 		EntityManager em = emfactory.createEntityManager();
-		List<Details> allItems = em.createQuery("SELECT i FROM Details i").getResultList();
-		return allItems;
+		List<Details> allCars = em.createQuery("SELECT i FROM Details i").getResultList();
+		return allCars;
 	}
 	
 	public void updateItem(Details toEdit) {
-		// TODO Auto-generated method stub
+		// this is used to update the item in the database 
+		//most of the logic for this method is inside the StartProgram file
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
 		em.merge(toEdit);
@@ -36,10 +40,11 @@ public class DetailsHelper {
 	}
 	
 	public void deleteItem(Details toDelete) {
+		// this method actually removes the record from the database
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
 		TypedQuery<Details> typedQuery = em.createQuery("select det from Details det where det.year = :selectedYear and det.make = :selectedMake and det.model = :selectedModel", Details.class);
-		//Substitute parameter with actual data from the toDelete item
+		//Substitute parameter with actual data from the toDelete item method
 		typedQuery.setParameter("selectedYear", toDelete.getYear());
 		typedQuery.setParameter("selectedMake", toDelete.getMake());
 		typedQuery.setParameter("selectedModel", toDelete.getModel());
@@ -53,13 +58,23 @@ public class DetailsHelper {
 		em.close();
 	}
 
-	public List<Details> searchForItemByItem(String itemName) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Details> searchForCarByMake(String carMake) {
+		/* 
+		 * This method searches for a car in the database my its make
+		 * The reason the data type for the function is list is because many cars can have one make 
+		 * So the method needs to be able to return all cars of that make
+		 */
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Details> typedQuery = em.createQuery("select det from Details det where det.make = :selectedMake", Details.class);
+		typedQuery.setParameter("selectedMake", carMake);
+		List<Details> foundItems = typedQuery.getResultList();
+		em.close();
+		return foundItems;
 	}
 
 	public Details searchForCarById(int idToEdit) {
-		// TODO Auto-generated method stub
+		// This method searches for a car by ID this is used even if one of the other search methods is called
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
 		Details found = em.
@@ -67,9 +82,21 @@ public class DetailsHelper {
 		em.close();
 		return found;
 	}
+	
+	public List<Details> searchForCarByModel(String carModel) {
+		// Searches for the car by model (See searchForCarByMake for a more detailed explanation)
+		// returns a list in case there is more than one result found
+		EntityManager em = emfactory.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Details> typedQuery = em.createQuery("select det from Details det where det.model = :selectedModel", Details.class);
+		typedQuery.setParameter("selectedModel", carModel);
+		List<Details> foundItems = typedQuery.getResultList();
+		em.close();
+		return foundItems;
+	}
 
 	public List<Details> searchForCarByYear(int carYear) {
-		// TODO Auto-generated method stub
+		// searches for the car by year return type is List
 		EntityManager em = emfactory.createEntityManager();
 		em.getTransaction().begin();
 		TypedQuery<Details> typedQuery = em.createQuery("select det from Details det where det.year = :selectedYear", Details.class);
@@ -80,6 +107,9 @@ public class DetailsHelper {
 	}
 	
 	public void cleanUp() {
+		// This is to close connections so they do not stay running
 		emfactory.close();
 	}
+
+	
 }
--- src/view/StartProgram.java
+++ src/view/StartProgram.java
@@ -7,12 +7,14 @@ import controller.DetailsHelper;
 import model.Details;
 
 public class StartProgram {
+	// class variable that I use to get user input 
 	static Scanner in = new Scanner(System.in);
+	// class variable that is an object of my DetailsHelper class so I can call methods from that class through this class
 	static DetailsHelper det = new DetailsHelper();
 	
 
-	private static void addAnItem() {
-		// TODO Auto-generated method stub
+	private static void addACar() {
+		// this method adds a car to the database
 		System.out.print("Enter the year the car was made: ");
 		int year = in.nextInt();
 		in.nextLine(); // this consumes the extra \n thrown by nextInt that was giving me a bug when having two nextLines after
@@ -21,12 +23,12 @@ public class StartProgram {
 		System.out.print("Enter the car make: ");
 		String make = in.nextLine();
 		Details toAdd = new Details(year, make, model);
-		det.insertItem(toAdd);
+		det.insertCar(toAdd);
 
 	}
 	
 	private static void deleteAnItem() {
-		// TODO Auto-generated method stub
+		// this method will get the year, model and make of a car to delete 
 		System.out.print("Enter the year to delete: ");
 		int year = in.nextInt();
 		in.nextLine(); // this consumes the extra \n thrown by nextInt that was giving me a bug when having two nextLines after
@@ -34,45 +36,65 @@ public class StartProgram {
 		String model = in.nextLine();
 		System.out.print("Enter the make to delete: ");
 		String make = in.nextLine();
+		// saving the car that I want to delete as a new object
 		Details toDelete = new Details(year, model, make);
+		// passing the new object to my deleteItem method
 		det.deleteItem(toDelete);
 
 	}
 	
 	private static void editAnItem() {
 		// TODO Auto-generated method stub
+		// asking the user  how they want to search for the car they're trying to edit
 		System.out.println("How would you like to search? ");
 		System.out.println("1 : Search by Year");
 		System.out.println("2 : Search by Make");
 		System.out.println("3 : Search by Model");
+		//getting the user inputed answer from the console
 		int searchBy = in.nextInt();
-		in.nextLine();
+		in.nextLine(); // included to consume the extra \n because this was cause me issues else where so I'd rather be safe than sorry
 		List<Details> foundItems;
 		if (searchBy == 1) {
+			//this part of the if-statement executes if the user entered search by year
 			System.out.print("Enter the car year: ");
+			//I won't need to grab the extra \n here because it won't cause any issues
 			int carYear = in.nextInt();
+			//calling my function from DetailsHelper that searches for a car based on its year
 			foundItems = det.searchForCarByYear(carYear);
 			
 		} else if (searchBy == 2){
+			// this part of the if-statement executes if the user entered search by make
 			System.out.print("Enter the car make: ");
 			String carMake = in.nextLine();
-			foundItems = det.searchForItemByItem(carMake);
+			System.out.println(carMake);
+			//calling my function from DetailsHelper that searches for a car based on its make
+			foundItems = det.searchForCarByMake(carMake);
 
 		} else {
+			// this part of the if-statement executes if the user entered search by model
 			System.out.print("Enter the car model: ");
 			String carModel = in.nextLine();
-			foundItems = det.searchForItemByItem(carModel);
+			System.out.println(carModel);
+			//calling my function from DetailsHelper that searches for a car based on its model
+			foundItems = det.searchForCarByModel(carModel);
 		}
 
 		if (!foundItems.isEmpty()) {
+			// this if statement means if foundItems variable is not empty execute the body of the statement
 			System.out.println("Found Results.");
+			//for loop to get the id and convert to string in case there is more than one result found
+			//such as if there happens to be multiple cars from 2020 it will print their id so you can select the one you are looking for
 			for (Details d : foundItems) {
 				System.out.println(d.getId() + " : " + d.toString());
 			}
+			// having the user choose which of the possible multiple returns they wish to edit
 			System.out.print("Which ID to edit: ");
 			int idToEdit = in.nextInt();
-
+			// again this line is included because I was getting a weird bug that put the inputs after on the same line and would break the program
+			in.nextLine();
+			//calling my method from DetailsHelper passing the id the user entered as a parameter
 			Details toEdit = det.searchForCarById(idToEdit);
+			// this next print statement shows the year, make and model of the car retrieved by id
 			System.out.println("Retrieved " + toEdit.getYear() + " " + toEdit.getModel() + " " + toEdit.getMake());
 			System.out.println("1 : Update Year");
 			System.out.println("2 : Update Make");
@@ -81,29 +103,33 @@ public class StartProgram {
 			in.nextLine();
 
 			if (update == 1) {
+				// update year
 				System.out.print("New Year: ");
 				int newYear = in.nextInt();
 				toEdit.setYear(newYear);
 			} else if (update == 2) {
+				// update make
 				System.out.print("New Make: ");
 				String newMake = in.nextLine();
 				toEdit.setMake(newMake);
 			} else if (update == 3) {
+				// update model
 				System.out.print("New Model: ");
 				String newModel = in.nextLine();
-				toEdit.setMake(newModel);
+				toEdit.setModel(newModel);
 			}
 
 			det.updateItem(toEdit);
 
 		} else {
+			// this is called if the foundItems variable is empty
 			System.out.println("---- No results found");
 		}
 	}
 	
-	private static void viewTheList() {
-		// TODO Auto-generated method stub
-		List<Details> allItems = det.showAllItems();
+	private static void viewTheCars() {
+		// This method shows all the cars currently in the database
+		List<Details> allItems = det.showAllCars();
 		for(Details singleItem : allItems) {
 			System.out.println(singleItem.returnCarDetails());
 		}
@@ -114,6 +140,7 @@ public class StartProgram {
 	}
 	
 	public static void runMenu() {
+		// this is the method which controls the program and is used as an interface for the user
 		boolean goAgain = true;
 		System.out.println("--- Welcome to our awesome car detail list! ---");
 		while (goAgain) {
@@ -125,19 +152,20 @@ public class StartProgram {
 			System.out.println("*  5 -- Exit the program");
 			System.out.print("*  Your selection: ");
 			int selection = in.nextInt();
-			in.nextLine();
+			in.nextLine(); // eating my \n
 
 			if (selection == 1) {
-				addAnItem();
+				addACar();
 			} else if (selection == 2) {
 				editAnItem();
 			} else if (selection == 3) {
 				deleteAnItem();
 			} else if (selection == 4) {
-				viewTheList();
+				viewTheCars();
 			} else {
+				// calling my clean up method
 				det.cleanUp();
-				System.out.println("   Goodbye!   ");
+				System.out.println("   See ya!   ");
 				goAgain = false;
 			}
 
