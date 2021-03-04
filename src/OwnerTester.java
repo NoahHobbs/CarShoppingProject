a3d46b03f8ccc31252651e1060c905b59a7a2dc5
Noah Hobbs <mlzgphoenix@gmail.com>
2021-03-04 14:09:06 -0600

    added helper class

--- src/OwnerTester.java
+++ src/OwnerTester.java
@@ -2,19 +2,25 @@
 
 import java.util.List;
 
+import controller.ListDetailsHelper;
 import controller.OwnerHelper;
+import model.ListDetails;
 import model.Owner;
 
 public class OwnerTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Owner greg = new Owner("Greg");
+		Owner cameron = new Owner("Cameron");
 		OwnerHelper oh = new OwnerHelper();
-		oh.insertOwners(greg);
-		List<Owner> allOwners = oh.showAllOwners();
-		for(Owner o: allOwners) {
-			System.out.println(o.toString());
+		oh.insertOwners(cameron);
+		ListDetailsHelper ldh = new ListDetailsHelper();
+		ListDetails cameronList = new ListDetails("Cameron's List", cameron);
+		ldh.insertNewListDetails(cameronList);
+		List<ListDetails> allLists = ldh.getLists();
+		
+		for(ListDetails a: allLists) {
+			System.out.println(a.toString());
 		}
 	}
 
--- src/model/ListDetails.java
+++ src/model/ListDetails.java
@@ -2,13 +2,17 @@ package model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 @Entity
 @Table(name="list_details")
@@ -22,6 +26,9 @@ public class ListDetails {
 	@ManyToOne
 	@JoinColumn(name="OWNER_ID")
 	private Owner owner;
+	@OneToMany(cascade=CascadeType.PERSIST, fetch=FetchType.EAGER)
+	@JoinTable( name ="CARS_ON_LIST", joinColumns= {@JoinColumn(name="LIST_ID", referencedColumnName="LIST_ID")}, 
+	inverseJoinColumns= { @JoinColumn(name="CAR_ID", referencedColumnName="ID", unique=true) } )
 	private List<Details> listOfCars;
 	
 	public ListDetails() {
